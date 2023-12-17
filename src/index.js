ddb46f1a7701e05c93d4ca29c0478a0daa881438
Talentm36 <149770117+Talentm36@users.noreply.github.com>
2023-12-17 23:33:20 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 function updateWeather(response)
 {let temperatureElement=document.querySelector("#app-temperature");
 let appTemperature=response.data.temperature.current;
+let descriptionElement=document.querySelector("#description");
+let humidityElement=document.querySelector("#humidity");
+let windElement=document.querySelector("#wind-speed");
+windElement.innerHTML=`${response.data.wind.speed}km/h`
+humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+descriptionElement.innerHTML=response.data.condition.description;
 temperatureElement.innerHTML=Math.round(appTemperature);}
 
 
@@ -24,3 +30,38 @@ searchCity(inputSearch.value)
 
 let searchFormElement= document.querySelector("#search-input");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
